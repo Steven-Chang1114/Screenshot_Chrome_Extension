219e46040503984e4d8653af57e9a567c8faa453
Steven Chang <1632460272@qq.com>
2020-04-21 02:32:54 +0100

    functioning successfully

--- src/app.js
+++ src/app.js
@@ -3,11 +3,11 @@ import html2canvas from "html2canvas"
 //Receive msg from background
 chrome.runtime.onMessage.addListener((request, sender, sendRequest) => {
     if(request === "screenshot"){
+
         html2canvas(document.body).then(canvas => { 
 
         //Took screenshot
         var image = new Image();
-        //document.body.appendChild(image);
         image.src = canvas.toDataURL("image/png");
         image.id = "facial"
         var output=image.src
@@ -15,9 +15,13 @@ chrome.runtime.onMessage.addListener((request, sender, sendRequest) => {
         //Autosave to the local folder
         const anchor = document.createElement('a');
         anchor.download = "img.png";
-        anchor.href = output;
+        anchor.href = canvas.toDataURL("image/png");
         anchor.click()
+        })
+        .catch(e => {
+            console.log("Error " + e + ", the screenshot is not able to be taken")
+        })
     
-    });
-    }
+    };
+
 })
