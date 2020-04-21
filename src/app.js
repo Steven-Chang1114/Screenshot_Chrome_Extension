e75d416c54f046d7786315285a68d7f282f005c5
Steven Chang <1632460272@qq.com>
2020-04-21 02:21:48 +0100

    implement screenshot

--- src/app.js
+++ src/app.js
@@ -1,31 +1,23 @@
 import html2canvas from "html2canvas"
-console.log("FR&A starts")
 
 //Receive msg from background
 chrome.runtime.onMessage.addListener((request, sender, sendRequest) => {
-    console.log(request)
-    console.log("eede")
     if(request === "screenshot"){
-        // html2canvas(document.body).then(canvas => {
-    //     const text = document.createElement('h1');
-    //     text.textContent = "Successful"
-    //     text.style.color = "#888888;"
-    //     text.style.textAlign = "center";
-    //     document.body.appendChild(text);  
+        html2canvas(document.body).then(canvas => { 
 
-    //     //Took screenshot
-    //     var image = new Image();
-    //     //document.body.appendChild(image);
-    //     image.src = canvas.toDataURL("image/png");
-    //     image.id = "facial"
-    //     var output=image.src
+        //Took screenshot
+        var image = new Image();
+        //document.body.appendChild(image);
+        image.src = canvas.toDataURL("image/png");
+        image.id = "facial"
+        var output=image.src
     
-    //     //Autosave to the local folder
-    //     const anchor = document.createElement('a');
-    //     anchor.download = "img.png";
-    //     anchor.href = output;
-    //     anchor.click()
+        //Autosave to the local folder
+        const anchor = document.createElement('a');
+        anchor.download = "img.png";
+        anchor.href = output;
+        anchor.click()
     
-    // });
+    });
     }
 })
